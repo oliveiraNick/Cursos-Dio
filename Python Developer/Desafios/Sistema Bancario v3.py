4366be65dc50737419949f698496889c290a4891
Nicolas Oliveira <cp.nicolasoliveira@gmail.com>
2024-04-24 20:46:15 -0300

    Atualização Código
    Atualização do código da 3ª versão do sistema bancário.

--- Python Developer/Desafios/Sistema Bancario v3.py
+++ Python Developer/Desafios/Sistema Bancario v3.py
@@ -9,9 +9,11 @@ class Conta:
         self._cliente = cliente
         self._historico = historico
 
+
+    #configurando o @classmethod e o acesso aos atributos privados da conta
     @classmethod
     def nova_conta(cls, cliente, numero):
-        return cls(numero, cliente)
+        return cls (numero, cliente)
 
     @property
     def saldo(self):
@@ -32,6 +34,28 @@ class Conta:
     @property
     def historico(self):
         return self._historico
+
+
+    #Configurando as funções de saque e depósito da conta
+    def saque(self, valor:float):
+        excedeu_saldo = valor > self.saldo
+
+        if excedeu_saldo:
+            print("\033[31mSaldo insuficiente! Tente outro valor ou adicione dinheiro a sua conta.\033[m")
+        
+        elif valor > 0:
+            self._saldo -= valor
+            print(f"\033[32mValor \033[4mR${valor:.2f}\033[m\033[32m retirado com sucesso!\033[m")
+
+            return True
+
+        else:
+            print("\033[31mValor inválido, tente novamente!\033[m")
+        
+        return False
+
+    def deposito(self, ):
+        ...
 
 
 class Cliente:
